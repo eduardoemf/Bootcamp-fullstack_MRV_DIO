4640bae5b625517a17eddd4b7305bfd3493fee61
eduardoemf <88982857+eduardoemf@users.noreply.github.com>
2022-01-14 16:32:23 -0300

    Inclusão de módulo finalizado
    Finalização do curso de introdução a Java Script

---
+++ 10 Introdução ao JavaScript/aulas iniciais/testes_funcoes.js
@@ -0,0 +1,6 @@
+function soma (a, b) {
+    //console.log(a + b); usado para verificar os valores no debug
+    return a + b
+}
+
+soma(3, 5);
---
+++ 10 Introdução ao JavaScript/aulas iniciais/testes_manipulando um arquivo.js
@@ -0,0 +1,10 @@
+// comentário de uma linha
+
+/*
+comentário de várias linhas 
+*/
+
+// constantes são valores imutáveis no código javascript
+
+/* variáveis são itens que ocupam espaço na memória e podem ter mudança
+ nos valores no decorrer do código */
