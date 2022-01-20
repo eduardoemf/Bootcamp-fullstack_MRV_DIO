cda709590ee60027d532761bc8cddc12324e2908
eduardoemf <88982857+eduardoemf@users.noreply.github.com>
2022-01-20 17:32:52 -0300

    Subindo o trabalho inicial com ReactJS
    Atualização do módulo 31

---
+++ 31 Trabalhando com Componentes em React/react-dio/src/App.js
@@ -0,0 +1,20 @@
+import Item from './components/Item';
+import Card from './components/Card';
+
+const App = () => {
+  return (
+    <>
+    <h1>Minha primeira aplicação com React</h1>
+    <ul>
+        <Item texto='Item 1' />
+        <Item texto='Item 2' />
+        <Item texto='Item 3' />
+    </ul>
+    <Card/>
+    </>
+  )
+}
+
+export default App;
+
+// Método padrão seria uma função anônima
