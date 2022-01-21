3b624d2e57104468abbede833320e002e720b076
eduardoemf <88982857+eduardoemf@users.noreply.github.com>
2022-01-21 14:33:48 -0300

    Finalização do curso de intro a reactJS
    Subindo atualização do módulo 33

---
+++ 33 States e Effects no ReactJs/aula-hooks/src/components/App/App.js
@@ -0,0 +1,15 @@
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
+import SmartCounter from '../Smartcounter/Smartcounter';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <h1>Ifood</h1>
+      <IfoodCounter/>
+    </>
+  );
+  }
+
+export default App;
